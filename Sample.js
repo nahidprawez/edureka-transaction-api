e39d5af1abb637c0c71c9bfa61939d254a33d881
nahidprawez <nahid.prawez@gmail.com>
2022-10-09 15:12:30 +0530

    first commit

---
+++ Sample.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+
+
+
+//Run this file using the Node: command -> node Sample.js
---
+++ test.js
@@ -0,0 +1,8 @@
+//This is a test file...
+
+function myText() {
+    console.log("How are you?");
+}
+
+
+myText();
